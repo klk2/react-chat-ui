a43a77ddfe2646f86b70b58e8b588f969d1ff003
klk2 <tianlele_xl@163.com>
2019-12-26 15:10:48 +0800

    Update index.tsx

--- src/ChatBubble/index.tsx
+++ src/ChatBubble/index.tsx
@@ -28,7 +28,6 @@ export default class ChatBubble extends React.Component {
             ...styles.chatbubble,
             ...bubblesCentered ? {} : styles.chatbubbleOrientationNormal,
             ...chatbubble,
-            ...userBubble,
           }
         : {
             ...styles.chatbubble,
